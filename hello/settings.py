361778136a50ea38596c0e2ad7849eb349bc67b3
Ccdrisya <ccdrisya@gmail.com>
2024-11-30 13:22:18 +0530

    3

--- hello/settings.py
+++ hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7d4ce#rpe6ce5+(eokf90fc*%*r!@h=$(x^l3d_6vz*$qj!=88
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
